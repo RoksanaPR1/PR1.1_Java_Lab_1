d41768fe98b83b4e03cbb757b8e9537451982d38
RoksanaBalcerzak <rocksana0110@gmail.com>
2021-12-18 02:57:14 +0100

    Ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,14 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Wypisanie proste ze znakiem nowej linii na końcu");
+        System.out.println("Stringi" + " " + "można" + " " + "łączyć");
 
+        System.out.printf("Do stringa %s dodawać %s argumenty różnego typu jak np. liczby: %d%s", "można", "liczne", 777, "\n");
+
+        System.out.print("Z kolei ");
+        System.out.print("ten format zostawia ");
+        System.out.print("kursor na swoim miejscu.");
 
     }
 }
