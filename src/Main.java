43023b373ef73647612c3302a2bab5e5e2430cdb
RoksanaBalcerzak <rocksana0110@gmail.com>
2021-12-18 03:16:17 +0100

    Treść ćwiczenia 3

--- src/Main.java
+++ src/Main.java
@@ -64,5 +64,17 @@ public class Main {
         System.out.println("Dzielenie liczb zmiennoprzecinkowych: " + (liczbaX / liczbaY));
         System.out.println("Dzielenie modulo liczb zmiennoprzecinkowych: " + (liczbaX % liczbaY));
 
+        /* ćw.3 */
+
+        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
+         *   a) Imię, nazwisko, wiek, nr indeksu,
+         *   dane mają być wprowadzane z klawiatury w konsoli
+         *   b) wyświetlić dane za pomocą println i printf
+         */
+
+
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+         *   wynik wyświetlić w konsoli programu
+         */
     }
 }
