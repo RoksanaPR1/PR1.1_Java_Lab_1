4f18a36081092f2559a728eaf191a18e90705d03
RoksanaBalcerzak <rocksana0110@gmail.com>
2021-12-18 03:39:23 +0100

    Treść ćwiczenia 5

--- src/Main.java
+++ src/Main.java
@@ -166,5 +166,18 @@ public class Main {
         System.out.println("(y >= x && c) : " + (y >= x && c)); // true
         System.out.println("(a == b || b == c) && (x > y || y > z) : " + ((a == b || b == c) && (x > y || y > z))); // false
 
+        /* ćw.5 */
+
+        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
+         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
+         * */
+
+
+        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+
+
+        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
+         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+
     }
 }
