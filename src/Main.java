1478771f88b6843842799d8cf60f710a5bb4739b
RoksanaBalcerzak <rocksana0110@gmail.com>
2021-12-18 03:00:58 +0100

    Treść ćwiczenia 2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
         System.out.println("Witaj świecie!");
 
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+        //ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
+        //Użyć każdej z opcji
 
         System.out.println("Wypisanie proste ze znakiem nowej linii na końcu");
         System.out.println("Stringi" + " " + "można" + " " + "łączyć");
@@ -15,6 +15,20 @@ public class Main {
         System.out.print("Z kolei ");
         System.out.print("ten format zostawia ");
         System.out.print("kursor na swoim miejscu.");
+
+        int zmiennaInt = 7;
+        System.out.println("zmienna int = " + zmiennaInt);
+        /* ćw.2 */
+
+        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+
+        /*
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+
 
     }
 }
